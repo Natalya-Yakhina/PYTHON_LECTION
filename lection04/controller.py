42ceac34f59290b1a66a04733fbe146b5095e07c
Яхина Наталья Андреевна <  @vvrz.local>
2022-09-26 16:03:48 +0300

    калькулятор

--- lection04/controller.py
+++ lection04/controller.py
@@ -1,9 +1,9 @@
-import model_mult as model
+import model_sub as model
 import view
 
 def button_click(): # метод нажатия на кнопку
     value_a = view.get_value()
     value_b = view.get_value()
     model.init(value_a, value_b) # инициализация начальных значений
-    result = model.mult()
-    view.view_data(result, "resul")
+    result = model.do_it()
+    view.view_data(result, "result")
